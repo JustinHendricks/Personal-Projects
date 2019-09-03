d9fc087b11d26c2ec0d99dde6fc8f1e42a83b44c
JustinHendricks <47921340+JustinHendricks@users.noreply.github.com>
2019-09-03 19:09:27 -0400

    Update coin_change.py
    Commented code

--- IDLE Programs/coin_change.py
+++ IDLE Programs/coin_change.py
@@ -1,8 +1,14 @@
-import random
+#Code written 8/28/19
+#Inspired by this video https://youtu.be/HWW-jA6YjHk?list=PLC260E5J31yEtHISBAAFdGn-TnbNCxPbC
+
+#The idea behind solving this problem was inspired by another project in this repository 'fantasy inventory'. The program sees how much money is left and distributes change based on that, at the end it displays a tally of what coins you need to give exact change
+
 def num_coins(cents):
-    change = {'quarter': 0, 'dime': 0, 'nickel' : 0, 'penny': 0}
-    num_coins = 0
-    change_given = 0
+    change = {'quarter': 0, 'dime': 0, 'nickel' : 0, 'penny': 0} #Creates dictionary to keep track of how many of each coin you need
+    num_coins = 0 #Keeps track of how many coins you have in total
+    change_given = 0 #Not neccassary, just to make sure your input matches the change give
+#Each loop checks if cents(The amount of change left to give) is greater than the value of the coin(25, 10, etc.)
+#While that is the case, the program subtracts that cents' value, adds the cents' value to 'change_given', and adds 1 to its dictionary value
     while int(cents) >= 25:
          cents = int(cents) - 25
          change['quarter'] += 1
