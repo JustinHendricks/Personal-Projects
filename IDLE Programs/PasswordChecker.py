5732eba9eb6c38623a6d8587543a785c942f4767
JustinHendricks <47921340+JustinHendricks@users.noreply.github.com>
2019-09-03 12:42:18 -0400

    Update PasswordChecker.py
    Commented the code, and made some improvements:
    - Previously used a numerical check for each condition of a good password
    - Eg. if len(password) >= 8: --> lencheck += 1
    - if lencheck = 1: --> score += 1
    - If score == 4 (#one point for each condition met): --> password is secure
    -This lead to a lot of excess code that has been corrected

--- IDLE Programs/PasswordChecker.py
+++ IDLE Programs/PasswordChecker.py
@@ -1,42 +1,32 @@
-import random
-import string
-run = 0
+#Code written 8/24/19, Heavily edited 9/3/19
+#Automate the Boring Stuff / Chapter 7 / Practice Problem 1: Strong Password Detection
+
+#Expansion Ideas: add function that uses passwordChecker to try randomly generated passwords and print succesful ones as suggestions
+import random, string #Libraries are used to generate test passwords. Not needed if yo are just using inputs.
+run = 0 #Used to control the number of suggested passwords
 def passwordChecker(password):
-    checkr = {'length': False, 'uppercase': False, 'lowercase': False, 'digit': False}
-    score = 0
-    upcheck = 0
-    lowcheck = 0
-    digcheck = 0
-    if len(password) >= 8:
-        checkr['length'] = True
-        score += 1
-    for i in password:
+    checkr = {'length': False, 'uppercase': False, 'lowercase': False, 'digit': False} #Used to show what conditions need to be met for password to be secure
+    if len(password) >= 8: #Checks to see if the length of the password is at leats 8 characters
+        checkr['length'] = True #Checks length condition as true
+    for i in password: #Goes through every charcter in the password
         if i.isupper() == True:
-            upcheck += 1
+            checkr['uppercase'] = True #If the character is uppercase, its passed as True
         if i.islower() == True:
-            lowcheck += 1
+            checkr['lowercase'] = True #If the character is lowercase, its passed as True
         if i.isdigit() == True:
-            digcheck += 1
-    if upcheck > 0:
-        checkr['uppercase'] = True
-        score += 1
-    if lowcheck > 0:
-        checkr['lowercase'] = True
-        score += 1
-    if digcheck > 0:
-        checkr['digit'] = True
-        score += 1
+            checkr['digit'] = True #If the character is a digit, its passed as True
     for k, v in checkr.items():
-       print(str(k) + ': ' + str(v))
-    if score == 4:
-        print("Congrats! Your password is secure!")
+       print(str(k) + ': ' + str(v)) #Printes the dictionary in order to show what conditions haven't been met
+    if checkr['length'] == True and checkr['uppercase'] == True and checkr['lowercase'] == True and checkr['digit'] == True:
+        print("Congrats! Your password is secure!") #If all conditions are met it tells you the password is secure
     else:
         print("Warning! The password you typed is not secure!")
         print("It is reccomended that your password is at least 8 characters long, and that you have at least 1 capital letter, 1 lowercase letter, and 1 digit")
-
-while run != 10:
-    lent = random.randint(1,12)
-    password = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(lent)])
+        #If all conditions are not met, it prints out the conditions for a good password
+        #Expansion Ideas: See which condition/s is/are false and print out what the password specifically needs
+while run != 10: #Runs this 10 times
+    lent = random.randint(1,12) #Picks a random number of characters for the password to have. This is to test length in checkr.
+    password = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(lent)]) #Makes the password a random series of letters and numbers
     print(password)
-    passwordChecker(password)
-    run += 1
+    passwordChecker(password) #Shows and checks the password
+    run += 1 #Runs this loop again
