cc797dcef05e46df33ef5a97c0f41e783999100e
JustinHendricks <47921340+JustinHendricks@users.noreply.github.com>
2019-09-04 12:56:49 -0400

    Update collatz.py
    Commented code, fixed an error

--- IDLE Programs/collatz.py
+++ IDLE Programs/collatz.py
@@ -1,21 +1,23 @@
+#Code written 8/11/19
+#Automate the Boring Stuff / Chapter 3 / Practice Problem 1: The Collatz Sequence
+
 run = True
 def collatz(number):
-    while number % 2 != 0:
-        number = number * 3 + 1
-        print (number)
-        number = number / 2
-        print (number)
-    while number % 2 == 0:
-        number = number / 2
-        print (number)
-    if number == 1:
-        print('done')
-print('pick a number')
+   while number != 1: #Runs function until number == 1
+        if number % 2 != 0: #Checks if number is odd
+            number = number * 3 + 1
+            print (number)
+            number = number / 2
+            print (number)
+        if number % 2 == 0: #Checks if number is even
+            number = number / 2
+            print (number)
+    print('done')
+print('Enter number')
 while run == True:
     try:
         number = int(input())
         collatz(number)
         run = False
     except ValueError:
-        print('Please enter a number')
-    
+        print('Please enter a number') #Prevents user from inputting a string
