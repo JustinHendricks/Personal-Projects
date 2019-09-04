d943a6fd94187eda1ea2fabbc2daa94bcb3e5976
JustinHendricks <47921340+JustinHendricks@users.noreply.github.com>
2019-09-04 13:01:44 -0400

    Update strip.py
    Commented

--- IDLE Programs/strip.py
+++ IDLE Programs/strip.py
@@ -1,7 +1,9 @@
-
+#Code written 8/29/19
+#Automate the Boring Stuff / Chapter 7 / Practice Problem 2: Regex Version of strip()
 def strip():
     print("Type string that you will strip")
     string = input()
     print("Type character/s you wish to remove")
     char = input()
     print(string.replace(char, ''))
+#Code is self explanitory, it finds the character/s you want to remove in the string you typed and prints the new string
